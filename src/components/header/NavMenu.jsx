7c06968707576d2879fa43e4350ce85246c0a303
JulietaEsains <jesains@gmail.com>
2023-01-02 14:06:28 -0300

    secciones más dinámicas

--- src/components/header/NavMenu.jsx
+++ src/components/header/NavMenu.jsx
@@ -1,36 +1,27 @@
 import UilTimes from "@iconscout/react-unicons/icons/uil-times";
-import NavItem from '../header/NavItem'
+import NavItem from "../header/NavItem";
+import { sections as data } from "../../data/sections";
 import "../../styles/header/navMenu.css";
+import { useEffect, useState } from "react";
 
 function NavMenu({ menuShown, toggleMenuShown }) {
+  const [sections, setSections] = useState([]);
+
+  useEffect(() => {
+    setSections(data);
+  }, []);
+
   return (
     <div className={menuShown ? "nav__menu show-menu" : "nav__menu"}>
       <ul className="nav__list grid">
-        <NavItem
-          href="home"
-          section="Inicio"
-          toggleMenuShown={toggleMenuShown}
-        />
-        <NavItem
-          href="about"
-          section="Acerca de mí"
-          toggleMenuShown={toggleMenuShown}
-        />
-        <NavItem
-          href="skills"
-          section="Habilidades"
-          toggleMenuShown={toggleMenuShown}
-        />
-        <NavItem
-          href="qualification"
-          section="Trayectoria"
-          toggleMenuShown={toggleMenuShown}
-        />
-        <NavItem
-          href="projects"
-          section="Proyectos"
-          toggleMenuShown={toggleMenuShown}
-        />
+        {sections.map((section, id) => (
+          <NavItem
+            key={id}
+            href={section.href}
+            section={section.name}
+            toggleMenuShown={toggleMenuShown}
+          />
+        ))}
       </ul>
       <UilTimes className="nav__close" onClick={toggleMenuShown} />
     </div>
---
+++ src/data/sections.js
@@ -0,0 +1,27 @@
+export const sections = [
+  {
+    id: 1,
+    href: "home",
+    name: "Inicio",
+  },
+  {
+    id: 2,
+    href: "about",
+    name: "Acerca de mí",
+  },
+  {
+    id: 3,
+    href: "skills",
+    name: "Habilidades",
+  },
+  {
+    id: 4,
+    href: "qualification",
+    name: "Trayectoria",
+  },
+  {
+    id: 5,
+    href: "projects",
+    name: "Proyectos",
+  },
+];
